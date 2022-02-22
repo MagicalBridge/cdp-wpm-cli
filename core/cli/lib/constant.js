a92326706e42c2db1a39a271bb1dfa6c935a7d18
褚鹏飞 <540788769@qq.com>
2022-02-22 09:57:13 +0800

    faet: node

---
+++ core/cli/lib/constant.js
@@ -0,0 +1,5 @@
+const LOWEAST_NODE_VERSION = "12.0.0"
+
+module.exports = {
+  LOWEAST_NODE_VERSION
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,15 +1,32 @@
 "use strict"
 
+const semver = require("semver")
+const colors = require("colors/safe")
 const pkg = require("../package.json")
 const log = require("@cdp-wpm/log")
+const constant = require("./constant")
 
 function core() {
   // console.log("core")
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (error) {
+    log.error(error)
+  }
 }
 
 function checkPkgVersion() {
   log.info("cli", pkg.version)
+}
+
+function checkNodeVersion() {
+  // 获取当前版本号
+  let currentVersion = process.version
+  let loweastNodeVersin = constant.LOWEAST_NODE_VERSION
+  if (!semver.gte(currentVersion, loweastNodeVersin)) {
+    throw new Error(colors.red(`cdp-wpm需要安装 v${loweastNodeVersin}以上版本的 Node.js`)) 
+  }
 }
 
 module.exports = core

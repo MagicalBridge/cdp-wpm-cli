cf055cbd597b4afde4a8967bb94c215021a2ec89
褚鹏飞 <540788769@qq.com>
2022-03-01 08:29:04 +0800

    faet: 添加命令注册功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,8 +7,13 @@ const userHome = require("user-home")
 const pathExists = require("path-exists").sync
 const log = require("@cdp-wpm/log")
 const constant = require("./constant")
+const commander = require("commander")
+
 let args
 let config
+
+// 注册一个命令
+const program = new commander.Command()
 
 function core() {
   try {
@@ -20,19 +25,73 @@ function core() {
     // log.verbose("debug","动态修改log level")
     checkEnv()
     checkGlobalUpdate()
+    registerCommand()
   } catch (error) {
     log.error(error)
   }
 }
 
+// 注册命令方法
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0]) // 从 pkg 中
+    .usage("<command> [options]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false)
+
+  program.on("option:debug", function () {
+    const opts = program.opts()
+    const { debug } = opts
+    if (debug) {
+      process.env.LOG_LEVEL = "verbose"
+    } else {
+      process.env.LOG_LEVEL = "info"
+    }
+    log.level = process.env.LOG_LEVEL
+    // log.verbose("debug")
+  })
+
+  // 对未知命令的监听
+  program.on("command:*", function (obj) {
+    console.log(colors.red(`未知的命令：${obj[0]}`))
+    // console.log(obj); // ["test"]
+    const availableCommands = program.commands.map((cmd) => cmd.name)
+    if (availableCommands.length > 0) {
+      console.log(colors.green(`可用的命令${availableCommands.join(",")}`))
+    }
+  })
+
+  // 当输入的命令中参数小于3 说明没有执行命令
+  // console.log(process.argv)
+  // [
+  //   '/Users/louis/.nvm/versions/node/v14.16.0/bin/node',
+  //   '/Users/louis/.nvm/versions/node/v14.16.0/bin/cdp-wpm',
+  //   'test'
+  // ]
+  if (process.argv.length < 3) {
+    program.outputHelp()
+    console.log();
+  }
+
+  program.parse(process.argv)
+}
+
 // 检查最新的版本号
-function checkGlobalUpdate() {
+async function checkGlobalUpdate() {
   // 1、获取当前的版本号
   const currentVersion = pkg.version
   const npmName = pkg.name
   // 2、获取npm的信息
-  const { getNpmInfo } = require("@cdp-wpm/get-npm-info")
-  getNpmInfo(npmName)
+  const { getNpmSemverVersion } = require("@cdp-wpm/get-npm-info")
+  const lastVersion = await getNpmSemverVersion(currentVersion, npmName)
+
+  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
+    log.warn(
+      colors.yellow(
+        `请手动更新 ${npmName} 当前版本:${currentVersion}, 最新版本:${lastVersion}`
+      )
+    )
+  }
 }
 
 // 检查环境变量
@@ -45,6 +104,7 @@ function checkEnv() {
     })
   }
   config = createDefaultConfig()
+  // 打印缓存主目录
   console.log(process.env.CLI_HOME_PATH)
 }
 
--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -10,16 +10,60 @@ function getNpmInfo(npmName, registry) {
     return null
   }
 
-  const registryUrl = registry || getDefaultRegistry();
+  const registryUrl = registry || getDefaultRegistry()
+  const npmInfoUrl = urlJoin(registryUrl, npmName)
+
+  // 请求接口
+  return axios
+    .get(npmInfoUrl)
+    .then((response) => {
+      if (response.status === 200) {
+        // console.log(response.data)
+        return response.data
+      } else {
+        return null
+      }
+    })
+    .catch((err) => {
+      Promise.reject(err)
+    })
 }
 
-function getDefaultRegistry(isOriginal = false) {
+function getDefaultRegistry(isOriginal = true) {
   // 这里的镜像地址上次还出现过一次问题
   return isOriginal
     ? "https://registry.npmjs.org"
     : "https://registry.npmmirror.com"
 }
 
+async function getNpmVersions(npmName, registry) {
+  const data = await getNpmInfo(npmName, registry)
+  if (data) {
+    return Object.keys(data.versions)
+  } else {
+    return []
+  }
+}
+
+// 获取所有满足条件的版本号
+function getSemverVersions(baseVersion, versions) {
+  versions = versions.filter((version) => semver.gt(version, baseVersion))
+  return versions
+}
+
+async function getNpmSemverVersion(baseVersion, npmName, registry) {
+  // 拿到的是所有的版本号 这里返回的版本号 是 [0.0.2].... [0.0.14] 这种形式
+  const versions = await getNpmVersions(npmName, registry)
+  // 过滤出来新的版本号 都是
+  const newVersions = getSemverVersions(baseVersion, versions)
+  // console.log(newVersions);
+  if (newVersions && newVersions.length > 0) {
+    return newVersions[newVersions.length - 1]
+  }
+}
+
 module.exports = {
   getNpmInfo,
+  getNpmVersions,
+  getNpmSemverVersion,
 }

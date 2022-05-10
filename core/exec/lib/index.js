f58bf2a5ffde8f0184c9102e22e7769bcca4bfc3
褚鹏飞 <540788769@qq.com>
2022-05-10 09:26:55 +0800

    feat: 使用childprocess 解析参数

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,6 +1,8 @@
 "use strict"
 const Package = require("@cdp-wpm/package")
 const path = require("path")
+const cp = require("child_process")
+const log = require("@cdp-wpm/log")
 // const userHome = require("user-home")
 
 const SETTINGS = {
@@ -11,7 +13,7 @@ const SETTINGS = {
 const CATCH_DIR = "dependencies"
 
 async function exec(currOption, currentArgs) {
-  // 我们全局监听了 targetPath 这个options 然后放进了环境变量中 做了了业务逻辑的解耦
+  // 我们全局监听了 targetPath 这个options 然后放进了环境变量中 做了业务逻辑的解耦
   let targetPath = process.env.CLI_TARGET_PATH
   // 设置缓存路径
   let storePath = ""
@@ -38,13 +40,14 @@ async function exec(currOption, currentArgs) {
       packageName,
       packageVersion,
     })
-    // 如果包存在的话 走的是更新pkg逻辑 
+    // 如果包存在的话 走的是更新pkg逻辑
     if (await pkg.exists()) {
       await pkg.update()
-    } else { // 不存在 安装包逻辑
+    } else {
+      // 不存在 安装包逻辑
       // install 返回的是一个promise 这是使用await保证同步执行逻辑
       await pkg.install()
-      console.log("包安装完毕=============");
+      console.log("包安装完毕=============")
     }
   } else {
     //targetpath 被指定了，可能是为了调试安装这个模块
@@ -54,7 +57,7 @@ async function exec(currOption, currentArgs) {
       packageVersion,
     })
   }
-  // console.log(pkg.getRootFilePath())
+  // console.log(pkg.getRootFilePath())  /Users/louis/Documents/myProject/cdp-wpm-cli/commands/init/lib/index.js
   // 最终会拿到这个文件，并且执行
   const rootFile = pkg.getRootFilePath()
   // 如果返回是一个function的话执行执行起来
@@ -63,7 +66,27 @@ async function exec(currOption, currentArgs) {
   // 如果有targetPath 使用这种方式调试
   // cdp-wpm init testproject -tp /Users/louis/Documents/myProject/cdp-wpm-cli/commands/init --debug --force
   if (rootFile) {
-    require(rootFile)()
+    try {
+      // require(rootFile).call(null, Array.from(arguments))
+      const code = "console.log(1)"
+      // 使用 spawn 利于node多进程的方式来执行命令
+      const child = cp.spawn("node", ["-e", code], {
+        cwd: process.cwd(),
+        stdio: "inherit", // 和父进程做通信
+      })
+
+      child.on("error", (e) => {
+        log.error(error.message)
+        process.exit(1)
+      })
+
+      child.on("exit", (e) => {
+        log.verbose("命令执行成功" + e)
+        process.exit(e)
+      })
+    } catch (error) {
+      log.error(error.message)
+    }
   } else {
     return null
   }
